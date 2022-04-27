5d73db406d92836e49e5015907b42f9361335e1e
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2022-04-27 11:23:46 +0530

    Update XeonCheems.js

--- XeonCheems.js
+++ XeonCheems.js
@@ -1,11 +1,6 @@
-//[© 2022 Xeon Bot Inc.]\\
+//[© 2022 JIMBRU v3 Inc.]\\
 
-//~you can re-upload but tag my channel
-//or put my channel link in the description.
-//~you can recode or copy the codes but give
-//credit.
-//~Thank you to LORD BUDDHA, ME AND MY 
-//FAMILY.
+// MIKHAIEL
 
 //[modules]\\
 require('./config')
